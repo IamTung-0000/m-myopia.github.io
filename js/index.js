deecae91e159626652345ff79d90efff18509d3c
m-myopia <60949015+m-myopia@users.noreply.github.com>
2021-05-19 04:34:33 +0700

    update

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://raw.githubusercontent.com/m-myopia/m-myopia.github.io/master/Main.json'
+	
+})
